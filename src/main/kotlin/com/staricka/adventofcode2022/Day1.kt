3a859f5e671e06cb3d0c8717d6cedefa2c672fa1
Mathias Staricka <mstaricka@gmail.com>
2022-11-30 21:06:37 -0800

    [day 1] initial

--- src/main/kotlin/com/staricka/adventofcode2022/Day1.kt
+++ src/main/kotlin/com/staricka/adventofcode2022/Day1.kt
@@ -1,13 +1,38 @@
 package com.staricka.adventofcode2022
 
+import java.util.PriorityQueue
+import kotlin.math.max
+
 class Day1 : Day {
   override val id = 1
 
   override fun part1(input: String) {
-    TODO("Not yet implemented")
+    var max = 0
+    var sum = 0
+    for (line in input.lines()) {
+      if (line.isBlank()) {
+        max = max(sum, max)
+        sum = 0
+      } else {
+        sum += line.toInt()
+      }
+    }
+    max = max(sum, max)
+    println(max)
   }
 
   override fun part2(input: String) {
-    TODO("Not yet implemented")
+    val max = PriorityQueue<Int>(Comparator.reverseOrder())
+    var sum = 0
+    for (line in input.lines()) {
+      if (line.isBlank()) {
+        max.add(sum)
+        sum = 0
+      } else {
+        sum += line.toInt()
+      }
+    }
+    max.add(sum)
+    println("${max.poll() + max.poll() + max.poll()}")
   }
 }
