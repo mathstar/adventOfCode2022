a44c28bfc0c5f22dba3f018b0f32597af38cc348
Mathias Staricka <mstaricka@gmail.com>
2022-12-06 11:47:47 -0800

    [day 6] refactored redundant code

--- src/main/kotlin/com/staricka/adventofcode2022/Day6.kt
+++ src/main/kotlin/com/staricka/adventofcode2022/Day6.kt
@@ -3,27 +3,19 @@ package com.staricka.adventofcode2022
 class Day6 : Day {
   override val id = 6
 
-  override fun part1(input: String): Any {
+  private fun String.findMarker(size: Int): Int {
     val buffer = ArrayDeque<Char>()
-    for ((i,c) in input.withIndex()) {
+    for ((i,c) in this.withIndex()) {
       buffer.add(c)
-      while (buffer.size > 4) buffer.removeFirst()
-      if (buffer.size == 4 && buffer.toSet().size == 4) {
+      while (buffer.size > size) buffer.removeFirst()
+      if (buffer.size == size && buffer.toSet().size == size) {
         return i + 1
       }
     }
-    throw Exception("packet not found")
+    throw Exception("marker not found")
   }
 
-  override fun part2(input: String): Any {
-    val buffer = ArrayDeque<Char>()
-    for ((i,c) in input.withIndex()) {
-      buffer.add(c)
-      while (buffer.size > 14) buffer.removeFirst()
-      if (buffer.size == 14 && buffer.toSet().size == 14) {
-        return i + 1
-      }
-    }
-    throw Exception("message not found")
-  }
+  override fun part1(input: String): Any = input.findMarker(4)
+
+  override fun part2(input: String): Any = input.findMarker(14)
 }
